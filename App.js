f7ce1c3ff8869f90e57cd31bfa6981dbdd6b46c0
Loran Maes <loran.maes@student.odisee.be>
2024-01-12 08:43:25 +0100

    Started test driven work

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config";
+import HomeRoot from "./screens/HomeRoot";
+import GoalScreenRoot from "./screens/GoalScreenRoot";
+import LogoutScreen from "./screens/LogoutScreen";
 
 export default function App() {
+  const Tab = createMaterialBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider config={config}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeRoot} options={{ tabBarIcon: "run" }}></Tab.Screen>
+          <Tab.Screen name="GoalRoot" component={GoalScreenRoot} options={{ tabBarIcon: "flag-checkered" }}></Tab.Screen>
+          <Tab.Screen name="Logout" component={LogoutScreen} options={{ tabBarIcon: "logout" }}></Tab.Screen>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
